b7d6fe23b0429c352660104cd754b737f6cd4d7d
Alex William <alexwilliamll@outlook.com>
2025-04-22 18:04:24 -0300

    work

--- onu_add.py
+++ onu_add.py
@@ -6,26 +6,26 @@ from tqdm import tqdm
 olt = Olt_pm()
 
 slot = 1
-pon = 14
+pon = 15
 vlan = 3000
 
 onts = [    
-    {"sn": "", "id": 30, "desc": "MIGRACAO_CTO"},
-    {"sn": "", "id": 31, "desc": "MIGRACAO_CTO"},
-    {"sn": "", "id": 32, "desc": "MIGRACAO_CTO"},
-    {"sn": "", "id": 33, "desc": "MIGRACAO_CTO"},
-    {"sn": "", "id": 34, "desc": "MIGRACAO_CTO"},
-    {"sn": "", "id": 35, "desc": "MIGRACAO_CTO"},
-    {"sn": "", "id": 36, "desc": "MIGRACAO_CTO"},
-    {"sn": "", "id": 37, "desc": "MIGRACAO_CTO"},
-    # {"sn": "", "id": 21, "desc": "MIGRACAO_CTO"},
-    # {"sn": "", "id": 22, "desc": "MIGRACAO_CTO"},
-    # {"sn": "", "id": 23, "desc": "MIGRACAO_CTO"},
-    # {"sn": "", "id": 24, "desc": "MIGRACAO_CTO"},
-    # {"sn": "", "id": 25, "desc": "MIGRACAO_CTO"},
-    # {"sn": "", "id": 29, "desc": "MIGRACAO_CTO"},
-    # {"sn": "", "id": 29, "desc": "MIGRACAO_CTO"},
-    # {"sn": "", "id": 29, "desc": "MIGRACAO_CTO"},
+    {"sn": "", "id": 7, "desc": "MIGRACAO_CTO"}, # 1
+    {"sn": "", "id": 8, "desc": "MIGRACAO_CTO"}, # 2
+    {"sn": "", "id": 9, "desc": "MIGRACAO_CTO"}, # 3
+    {"sn": "", "id": 10, "desc": "MIGRACAO_CTO"}, # 4
+    {"sn": "", "id": 11, "desc": "MIGRACAO_CTO"}, # 5
+    {"sn": "", "id": 12, "desc": "MIGRACAO_CTO"}, # 6 
+    # {"sn": "", "id": 31, "desc": "MIGRACAO_CTO"}, # 7
+    # {"sn": "", "id": 32, "desc": "MIGRACAO_CTO"}, # 8
+    # {"sn": "", "id": 33, "desc": "MIGRACAO_CTO"}, # 9
+    # {"sn": "", "id": 34, "desc": "MIGRACAO_CTO"}, # 10
+    # {"sn": "", "id": 23, "desc": "MIGRACAO_CTO"}, # 11
+    # {"sn": "", "id": 24, "desc": "MIGRACAO_CTO"}, # 12
+    # {"sn": "", "id": 25, "desc": "MIGRACAO_CTO"}, # 13
+    # {"sn": "", "id": 29, "desc": "MIGRACAO_CTO"}, # 14
+    # {"sn": "", "id": 29, "desc": "MIGRACAO_CTO"}, # 15
+    # {"sn": "", "id": 29, "desc": "MIGRACAO_CTO"}, # 16
 ]
 
 try:
@@ -66,10 +66,11 @@ for ont in onts:
 
 print("\n")
 # Salvar configuração
-print("SALVANDO AS ALTERAÇÕES NA OLT")
+# print("SALVANDO AS ALTERAÇÕES NA OLT")
 shell.send("save\n")
 shell.send("\n")  # Responde "Yes" à confirmação, se necessário
-time.sleep(120)  # Aguarda o save completar
+for _ in tqdm(range(120), desc="SALVANDO AS ALTERAÇÕES NA OLT"):
+        time.sleep(1)  # Aguarda o save completar
 output = shell.recv(65535).decode('utf-8')
 print(output)
 ssh.close()
