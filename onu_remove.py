56294ac14c4feeb48351f83584dcd451a3581e6b
Alex William <alexwilliamll@outlook.com>
2025-12-02 12:06:15 -0300

    ont-del-bl-jcs

--- onu_remove.py
+++ onu_remove.py
@@ -1,14 +1,16 @@
 import paramiko
 import time
-from olt_info import Olt
+from olt_info import Olt #jaicos
 from tqdm import tqdm
 
 olt = Olt()          
-slot = 1           
-pon = 8                
+slot = 
+pon = 
 
 # ONT IDS LIST
-ont_ids_to_remove = [41, 37, 60, 3, 53, 33, 54, 28, 8, 26, 39, 44, 66, 35, 62, 68, 18, 14, 38, 46, 22, 23, 25]  
+ont_ids_to_remove = [
+
+]  
 
 # SSH CONNECTION
 try:
@@ -44,12 +46,13 @@ for ont_id in ont_ids_to_remove:
     shell.send(f"ont delete {pon} {ont_id}\n")
     time.sleep(0.5)  # PAUSE BETWEEN COMMANDS TO AVOID OVERLOAD
 
-# EXIT CONFG MODE
+#EXIT CONFG MODE
 # shell.send("quit\n")
 # shell.send("save\n")
 # shell.send("\n")  # CONFIRM THE SAVE COMMAND, IF NECESSARY
-for _ in tqdm(range(120), desc="SALVANDO AS ALTERAÇÕES NA OLT"):
-        time.sleep(1)  # WAIT SAVING
+
+# for _ in tqdm(range(120), desc="SALVANDO AS ALTERAÇÕES NA OLT"):
+#         time.sleep(1)  # WAIT SAVING
 
 
 output = shell.recv(65535).decode('utf-8')
